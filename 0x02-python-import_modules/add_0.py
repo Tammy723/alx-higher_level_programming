e1aef8ca6c82ebb284ee868fc4e19d510f47e88a
Tammy723 <satti.f@gmail.com@example.com>
2024-01-04 12:43:14 -0800

    a program that imports the function from a file

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a ,b):
+    """My addition function
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        The Return value. a + b
+    """
+    return (a + b)
