bfa69491f0c14cc053daf78e58f53233f85dc8b8
Tammy723 <satti.f@gmail.com>
2024-01-22 12:22:14 +0000

    fixing task 4

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+"""Returns the division of a over b"""
     try:
-        divide = a / b
+        div = a / b
     except (ZeroDivisionError):
         div = None
     finally:
-        print("Inside result: {}".format(divide))
-        return divide
+        print("Inside result: {}".format(div))
+        return div
