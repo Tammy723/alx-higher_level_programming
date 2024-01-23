628de97687f136eb553c1933b7ba5d13f4220e2e
Tammy723 <satti.f@gmail.com>
2024-01-23 11:16:59 +0000

    fixing task 10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 """Define a MagicClass matching exactly a bytecode provided by Holberton."""
+
 import math
+
 
 class MagicClass:
     """Represent a circle."""
@@ -24,4 +26,3 @@ class MagicClass:
     def circumference(self):
         """Return The circumference of the MagicClass."""
         return (2 * math.pi * self.__radius)
-
